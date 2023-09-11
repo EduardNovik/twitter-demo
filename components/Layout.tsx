64234730ad50816d7fc649a63909841e57a917e5
Eduard <eddnovik@gmail.com>
2023-09-11 19:09:02 +0200

    create general layout of the app

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,5 @@
+import Sidebar from "./layout/Sidebar";
+import FollowBar from "./layout/FollowBar";
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -7,9 +9,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
+          <Sidebar />
           <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
             {children}
           </div>
+          <FollowBar />
         </div>
       </div>
     </div>
---
+++ components/Modal.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback } from "react";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+}
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+}) => {
+  const handleCLose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onClose();
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div
+      className="
+    
+    "
+    >
+      Modal
+    </div>
+  );
+};
+
+export default Modal;

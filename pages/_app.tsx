d9b1292c5124b2c05e0e49f92a3c3d37686781ae
Eduard <eddnovik@gmail.com>
2023-09-13 20:59:14 +0200

    add ui and zustand store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import Layout from "@/components/Layout";
-import Modal from "@/components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
